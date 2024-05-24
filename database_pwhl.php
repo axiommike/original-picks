ebf35f2fd29458958fa4d353533e0ce7190c43e4
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-05-24 17:09:10 -0600

    Update database_pwhl.php

--- database_pwhl.php
+++ database_pwhl.php
@@ -47,6 +47,7 @@
 
             <!--====== Table Pagination ======-->
             <div class="w3-bar w3-transparent">
+                <a href="database_fa.php" class="w3-button w3-right w3-text-black">Free Agents &#10095;</a>
                 <a href="database_nhl.php" class="w3-button w3-text-black">&#10094; NHL Database</a>
             </div>
 

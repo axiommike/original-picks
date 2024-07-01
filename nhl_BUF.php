b1326d3b7e34c398d57f848eb80e6d24d5824088
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-06-30 22:00:39 -0600

    Update nhl_BUF.php

--- nhl_BUF.php
+++ nhl_BUF.php
@@ -24,6 +24,26 @@
         .w3-responsive {
             padding: 20px;
         }
+
+        /* Table */
+        .custom-table {
+            width: 100%;
+            border-collapse: collapse;
+            padding: 30px;
+            border: 1px solid black; /* Transparent border */
+            background-color: white;
+        }
+
+        .custom-table th {
+            color: #fff; /* Change to your desired text color */
+            background-color: #003087;
+        }
+
+        /* Image Size */
+        .title-logo img {
+            width: 80px; 
+            height: 80px; 
+        }
     </style>
 
     <!--====== JS ======-->
@@ -36,7 +56,6 @@
 </head>
 
 <body>
-    
     <!--====== Background Image ======-->
     <header class="banner">
 
@@ -46,7 +65,13 @@
         <!--====== Table ======-->
         <div class="w3-responsive">
             <div class="title-logo">
-                <h2>BUFFALO SABRES</h2>
+                <h2>Buffalo Sabres</h2>
+
+                <div class="team-info">
+                    <h3><strong>Coach:</strong> Lindy Ruff</h3>
+                    <h3><strong>General Manager:</strong> Kevyn Adams</h3>
+                 </div>  
+
                 <img src="images/logo-sabres.png" alt="Title Logo">
             </div>
 
@@ -73,4 +98,3 @@
 
 </body>
 </html>
-           

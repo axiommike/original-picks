ad0681fcbb64083038e42bfa602f7b0d2b5e6ef7
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-06-30 22:14:34 -0600

    Update nhl_NJD.php

--- nhl_NJD.php
+++ nhl_NJD.php
@@ -24,6 +24,26 @@
         .w3-responsive {
             padding: 20px;
         }
+
+        /* Table */
+        .custom-table {
+            width: 100%;
+            border-collapse: collapse;
+            padding: 30px;
+            border: 1px solid black; /* Transparent border */
+            background-color: white;
+        }
+
+        .custom-table th {
+            color: #fff; /* Change to your desired text color */
+            background-color: #CE1126;
+        }
+
+        /* Image Size */
+        .title-logo img {
+            width: 80px; 
+            height: 80px; 
+        }
     </style>
 
     <!--====== JS ======-->
@@ -36,7 +56,6 @@
 </head>
 
 <body>
-    
     <!--====== Background Image ======-->
     <header class="banner">
 
@@ -47,6 +66,12 @@
         <div class="w3-responsive">
             <div class="title-logo">
                 <h2>New Jersey Devils</h2>
+
+                <div class="team-info">
+                    <h3><strong>Coach:</strong> Sheldon Keefe</h3>
+                    <h3><strong>General Manager:</strong> Tom Fitzgerald</h3>
+                 </div>  
+
                 <img src="images/logo-devils.png" alt="Title Logo">
             </div>
 
@@ -73,4 +98,3 @@
 
 </body>
 </html>
-           

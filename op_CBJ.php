f1147bbe5f9ddbb3e4f857128e6ebc11cd178914
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-07-03 14:45:52 -0600

    Update op_CBJ.php

--- op_CBJ.php
+++ op_CBJ.php
@@ -24,6 +24,26 @@
         .w3-responsive {
             padding: 20px;
         }
+
+        /* Table */
+        .custom-table {
+            width: 100%;
+            border-collapse: collapse;
+            padding: 30px;
+            border: 1px solid black; /* Transparent border */
+            background-color: white;
+        }
+
+        .custom-table th {
+            color: #fff; /* Change to your desired text color */
+            background-color: #002654;
+        }
+
+        /* Image Size */
+        .title-logo img {
+            width: 80px; 
+            height: 80px; 
+        }
     </style>
 
     <!--====== JS ======-->
@@ -46,8 +66,8 @@
         <div class="w3-responsive">
             <div class="title-logo">
                 <h2>Columbus Blue Jackets</h2>
-                <h2>ORIGINAL PICKS</h2>
-                <img src="images/logo-blueJackets.png" alt="Title Logo">
+
+                <img src="images/logo-bluejackets.png" alt="Title Logo">
             </div>
 
             <!--====== Table Pagination ======-->
@@ -73,6 +93,3 @@
 
 </body>
 </html>
-            
-
-          

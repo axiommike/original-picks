bfd956de566f1bf84c730743e1e63e93c847b61e
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-07-04 10:47:53 -0600

    Update op_DET.php

--- op_DET.php
+++ op_DET.php
@@ -66,6 +66,7 @@
         <div class="w3-responsive">
             <div class="title-logo">
                 <h2>Detroit Red Wings</h2>
+                <h2>ORIGINAL PICKS</h2>
 
                 <img src="images/logo-redwings.png" alt="Title Logo">
             </div>

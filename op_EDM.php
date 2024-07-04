373efee22a502c6304aa165637b28d05981fd418
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-07-04 10:47:42 -0600

    Update op_EDM.php

--- op_EDM.php
+++ op_EDM.php
@@ -66,6 +66,7 @@
         <div class="w3-responsive">
             <div class="title-logo">
                 <h2>Edmonton Oilers</h2>
+                <h2>ORIGINAL PICKS</h2>
 
                 <img src="images/logo-oilers.png" alt="Title Logo">
             </div>

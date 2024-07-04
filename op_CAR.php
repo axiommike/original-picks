37a3a6889e7c58fa851f4551ff280aa247f57b8f
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-07-04 10:48:47 -0600

    Update op_CAR.php

--- op_CAR.php
+++ op_CAR.php
@@ -66,6 +66,7 @@
         <div class="w3-responsive">
             <div class="title-logo">
                 <h2>Carolina Hurricanes</h2>
+                <h2>ORIGINAL PICKS</h2>
 
                 <img src="images/logo-hurricanes.png" alt="Title Logo">
             </div>

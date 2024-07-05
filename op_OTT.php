036509b83b1169ffc56c28e79447d56776d3eb9f
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-07-04 22:45:33 -0600

    Update op_OTT.php

--- op_OTT.php
+++ op_OTT.php
@@ -24,6 +24,26 @@
         .w3-responsive {
             padding: 20px;
         }
+
+        /* Table */
+        .custom-table {
+            width: 100%;
+            border-collapse: collapse;
+            padding: 30px;
+            border: 1px solid black; /* Transparent border */
+            background-color: white;
+        }
+
+        .custom-table th {
+            color: #fff; /* Change to your desired text color */
+            background-color: #000000;
+        }
+
+        /* Image Size */
+        .title-logo img {
+            width: 90px; 
+            height: 70px; 
+        }
     </style>
 
     <!--====== JS ======-->
@@ -46,7 +66,8 @@
         <div class="w3-responsive">
             <div class="title-logo">
                 <h2>Ottawa Senators</h2>
-                <h2>ORIGINAL PICKS</h2>
+                <h2>ORIGINAL PICKS</h2>   
+
                 <img src="images/logo-senators.png" alt="Title Logo">
             </div>
 
@@ -73,6 +94,3 @@
 
 </body>
 </html>
-            
-
-          

1c2fdf5b9be1a24cf1a62cc505e61d2ebad222cc
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-07-06 13:03:15 -0600

    Update table_nhlDatabase.php

--- table_nhlDatabase.php
+++ table_nhlDatabase.php
@@ -17,7 +17,7 @@ function generateTable($pdo, $title)
     echo "<tbody class='w3-text-white w3-centered' id='tableContent'>";
 
     foreach ($players as $index => $row) {
-        $backgroundColor = $index % 2 === 0 ? '#e9e9e9' : 'transparent';
+        $backgroundColor = $index % 2 === 0 ? '#e9e9e9' : '#e9e9e9';
         $textColor = 'black';
 
         // Text colours
@@ -53,7 +53,7 @@ echo '<script>
 $(document).ready(function() {
     $("#playerTable").DataTable({
         "order": [[8, "desc"]], 
-        "pageLength": 100,
+        "pageLength": 50,
         "dom": "<\'top\'fpi>rt<\'clear\'>"
     });
 });

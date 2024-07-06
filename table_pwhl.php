7c2b272d2dc5de728e0015d401816099a76e3e84
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-07-05 22:50:50 -0600

    Update table_pwhl.php

--- table_pwhl.php
+++ table_pwhl.php
@@ -25,7 +25,7 @@ function generateTable($pdo, $signedTeam, $position, $limit, $title, $averageCou
     echo "<h3>$title <span style='font-size: 20px;'>- " . number_format($averageRating) . "</span></h3>";
     echo "<table class='sortable custom-table'>";
     echo "<thead style='background-color: black; color: white;'>";
-    echo "<tr><th class='w3-center'>#</th><th class='w3-left-align'>&nbsp;&nbsp;&nbsp;Player Name</th><th class='w3-center'>Cap Hit</th><th class='w3-center'>Term</th><th class='w3-center'>Rights</th><th class='w3-center'>PWHL Rating</th></tr>";
+    echo "<tr><th class='w3-center'>#</th><th class='w3-left-align'>&nbsp;&nbsp;&nbsp;Player Name</th><th class='w3-center'>Drafted Team</th><th class='w3-center'>Cap Hit</th><th class='w3-center'>Term</th><th class='w3-center'>Rights</th><th class='w3-center'>PWHL Rating</th></tr>";
     echo "</thead>";
     echo "<tbody class='w3-text-white w3-centered' id='tableContent'>";
 
@@ -35,11 +35,13 @@ function generateTable($pdo, $signedTeam, $position, $limit, $title, $averageCou
 
         // Check if contract type is Two-Way
         $capHitTextColor = $row['contractType'] === 'Two-Way' ? '#228B22' : $textColor;
+        $draftedTeamColor = $row['draftedTeam'] === 'Undrafted' ? '#ff0000' : $textColor;
 
         // Displays table rows
         echo "<tr style='background-color: $backgroundColor;' onmouseover=\"this.style.backgroundColor='white';\" onmouseout=\"this.style.backgroundColor='$backgroundColor';\">";
         echo "<td style='color: $textColor;'>{$row['jersey']}</td>";
         echo "<td class='w3-left-align' style='color: $textColor;'>&nbsp;&nbsp;&nbsp;{$row['playerName']}  {$row['captain']}</td>";
+        echo "<td style='color: $draftedTeamColor;'>{$row['draftedTeam']}</td>";
         echo "<td style='color: $capHitTextColor;'>" . '$' . number_format($row['capHit'], 0, '', ',') . "</td>";
         echo "<td style='color: $textColor;'>{$row['term']}</td>";
         echo "<td style='color: $textColor;'>{$row['rights']}</td>";

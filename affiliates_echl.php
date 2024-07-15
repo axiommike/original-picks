64dacd8fe47fa9e9de7052a006384e746d50e880
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-07-15 14:39:47 -0600

    Update affiliates_echl.php

--- affiliates_echl.php
+++ affiliates_echl.php
@@ -19,6 +19,31 @@
             background-repeat: no-repeat;
             background-attachment: fixed;
         }
+
+        /*! Table Padding */
+        .w3-responsive {
+            padding: 20px;
+        }
+
+        /* Table */
+        .custom-table {
+            width: 100%;
+            border-collapse: collapse;
+            padding: 30px;
+            border: 1px solid black; /* Transparent border */
+            background-color: white;
+        }
+
+        .custom-table th {
+            color: #fff; /* Change to your desired text color */
+            background-color: #e1373c;
+        }
+
+        /* Image Size */
+        .title-logo img {
+            width: 90px; 
+            height: 80px; 
+        }
     </style>
 
     <!--====== JS ======-->
@@ -40,7 +65,12 @@
         <!--====== Table ======-->
         <div class="w3-responsive">
             <div class="title-logo">
-                <h2>ECHL Affiliates</h2>
+                <h2>East Coast Hockey League</h2>
+
+                <div class="team-info">
+                    <h3>Affiliation History</h3>
+                </div>
+
                 <img src="images/echl.png" alt="Title Logo">
             </div>
 

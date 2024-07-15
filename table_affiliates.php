5e65cb8403a685ce8c9cc030b26dca5ba9306520
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-07-15 14:40:05 -0600

    Update table_affiliates.php

--- table_affiliates.php
+++ table_affiliates.php
@@ -4,7 +4,7 @@ require("includes/db.inc.php");
 function generateTable($pdo, $league, $title)
 {
     // Pulls from the database
-    $stmt = $pdo->prepare('SELECT * FROM affiliation_history WHERE league = :league ORDER BY nhlTeam ASC, startDate ASC');
+    $stmt = $pdo->prepare('SELECT * FROM affiliation_history WHERE league = :league');
     $stmt->bindParam(':league', $league);
     $stmt->execute();
     $teams = $stmt->fetchAll();
@@ -18,11 +18,18 @@ function generateTable($pdo, $league, $title)
     echo "<tbody class='w3-text-white w3-centered' id='tableContent'>";
 
     foreach ($teams as $index => $row) {
-        $backgroundColor = $index % 2 === 0 ? '#e9e9e9' : 'transparent';
+        $backgroundColor = $index % 2 === 0 ? '#e9e9e9' : '#e9e9e9';
         $textColor = 'black';
 
         // Text colours
-        $statusColor = $row['teamStatus'] === 'Inacive' ? '#ff0000' : $textColor;
+        // Text colours
+        if ($row['teamStatus'] === 'Inactive') {
+            $statusColor = '#ff0000';
+        } elseif ($row['teamStatus'] === 'Active') {
+            $statusColor = '#228B22';
+        } else {
+            $statusColor = $textColor;
+        }
 
         // Displays table rows
         echo "<tr style='background-color: $backgroundColor;' onmouseover=\"this.style.backgroundColor='white';\" onmouseout=\"this.style.backgroundColor='$backgroundColor';\">";
@@ -46,8 +53,11 @@ echo '<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js
 echo '<script>
 $(document).ready(function() {
     $("#playerTable").DataTable({
-        "order": [[4, "desc"]], 
-        "pageLength": 100,
+        "order": [
+            [0, "asc"], // First column in ascending order
+            [1, "asc"] // Second column in descending order
+        ], 
+        "pageLength": 50,
         "dom": "<\'top\'fpi>rt<\'clear\'>"
     });
 });

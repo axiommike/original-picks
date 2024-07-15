28829b512e8394a230f310740ae8fff09ea3736a
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-07-15 15:12:27 -0600

    Update draft_2009.php

--- draft_2009.php
+++ draft_2009.php
@@ -24,6 +24,26 @@
         .w3-responsive {
             padding: 20px;
         }
+
+        /* Table */
+        .custom-table {
+            width: 100%;
+            border-collapse: collapse;
+            padding: 30px;
+            border: 1px solid black; /* Transparent border */
+            background-color: white;
+        }
+
+        .custom-table th {
+            color: #fff; /* Change to your desired text color */
+            background-color: #c41130;
+        }
+
+        /* Image Size */
+        .title-logo img {
+            width: 120px; 
+            height: 100px; 
+        }
     </style>
 
     <!--====== JS ======-->
@@ -56,7 +76,7 @@
             </div>
 
             <!--====== Forwards Table ======-->
-            <?php generateTable($pdo, '2009', 100, 'Draft Rankings'); ?>
+            <?php generateTable($pdo, '2009', 100, 'Drafted Players'); ?>
 
         </div>
     </header>

3e309a1c68527509fb4505e505d6037000d11ac8
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-07-16 07:17:20 -0600

    Update draft_2016.php

--- draft_2016.php
+++ draft_2016.php
@@ -76,7 +76,7 @@
             </div>
 
             <!--====== Forwards Table ======-->
-            <?php generateTable($pdo, '2016', 100, 'Drafted Players'); ?>
+            <?php generateTable($pdo, '2016', 300, 'Drafted Players'); ?>
 
         </div>
     </header>

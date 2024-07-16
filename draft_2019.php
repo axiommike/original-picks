e7c7372c7005b0f4a0b6ef045ddf152a1e826e60
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-07-16 07:12:11 -0600

    Update draft_2019.php

--- draft_2019.php
+++ draft_2019.php
@@ -24,6 +24,26 @@
         .w3-responsive {
             padding: 20px;
         }
+
+        /* Table */
+        .custom-table {
+            width: 100%;
+            border-collapse: collapse;
+            padding: 30px;
+            border: 1px solid black; /* Transparent border */
+            background-color: white;
+        }
+
+        .custom-table th {
+            color: #fff; /* Change to your desired text color */
+            background-color: #042458;
+        }
+
+        /* Image Size */
+        .title-logo img {
+            width: 120px; 
+            height: 100px; 
+        }
     </style>
 
     <!--====== JS ======-->
@@ -56,7 +76,7 @@
             </div>
 
             <!--====== Forwards Table ======-->
-            <?php generateTable($pdo, '2019', 200, 'Draft Rankings'); ?>
+            <?php generateTable($pdo, '2019', 100, 'Drafted Players'); ?>
 
         </div>
     </header>

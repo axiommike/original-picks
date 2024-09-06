5f413dda4c86cdabe05c204bdcf771ccbfc0c108
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-09-06 14:14:14 -0600

    Update op_MIN.php

--- op_MIN.php
+++ op_MIN.php
@@ -68,7 +68,7 @@
                 <h2>Minnesota Wild</h2>
                 
                 <div class="team-info">
-                    <h3>Original Picks</h3>
+                    <h4>Original Picks</h4>
                 </div>
 
                 <img src="images/logo-wild.png" alt="Title Logo">

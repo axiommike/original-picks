7341dcd31f3d29fdbd8b9cd3f41e4af54ef242ba
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-09-06 14:27:22 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
-  <!--====== CSS ======-->
-  <link rel="stylesheet" href="css/navbar.css">
-  <link rel="stylesheet" href="css/index.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  
-  <style>
-    /* Background Image */
-    .banner {
-        width: 100%;
-        min-height: 100vh;
-        background-image: url("images/ice.png");
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-    }
-  </style>
-  
-  <!--====== JS ======-->
-  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-  
-  <!--====== Page Title ======-->
-  <title>Home - NHL Original Picks</title>
+    <!--====== CSS ======-->
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/index.css">
+    <style>
+        /*! Background Image */
+        .banner {
+            width: 100%;
+            min-height: 100vh;
+            background-image: url("images/ice.png");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }
+    </style>
+
+    <!--====== JS ======-->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script> 
+
+    <!--====== Page Title ======-->
+    <title>Home - NHL Original Picks</title>
 </head>
 
 <body>

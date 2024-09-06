efa242b0f0e1634dfc3f8dd230e3b2f0fc641909
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-09-06 14:11:22 -0600

    Update op_ANA.php

--- op_ANA.php
+++ op_ANA.php
@@ -68,7 +68,7 @@
                 <h2>Anaheim Ducks</h2>
                 
                 <div class="team-info">
-                    <h3>Original Picks</h3>
+                    <h4>Original Picks</h4>
                 </div>
 
                 <img src="images/logo-ducks.png" alt="Title Logo">

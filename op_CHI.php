5be661e1feba63a8781133229ed1f46a5512a162
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-09-06 14:12:53 -0600

    Update op_CHI.php

--- op_CHI.php
+++ op_CHI.php
@@ -68,7 +68,7 @@
                 <h2>Chicago Blackhawks</h2> 
                 
                 <div class="team-info">
-                    <h3>Original Picks</h3>
+                    <h4>Original Picks</h4>
                 </div>
 
                 <img src="images/logo-blackhawks.png" alt="Title Logo">

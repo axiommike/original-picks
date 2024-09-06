450985b30276f773c36a926f6bc1ca54771a8106
afrank14 <147450402+afrank14@users.noreply.github.com>
2024-09-06 14:11:46 -0600

    Update op_ARI.php

--- op_ARI.php
+++ op_ARI.php
@@ -68,7 +68,7 @@
                 <h2>Arizona Coyotes</h2>
                 
                 <div class="team-info">
-                    <h3>Original Picks</h3>
+                    <h4>Original Picks</h4>
                 </div>
 
                 <img src="images/logo-coyotes.png" alt="Title Logo">

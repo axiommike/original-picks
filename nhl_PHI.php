1d2ac84fa7b3fe18dcf682d4d573f8300783b144
afrank14 <147450402+afrank14@users.noreply.github.com>
2025-06-28 17:55:55 -0600

    Update nhl_PHI.php

--- nhl_PHI.php
+++ nhl_PHI.php
@@ -68,8 +68,8 @@
                 <h2>Philadelphia Flyers</h2>
 
                 <div class="team-info">
-                    <h3><strong>Coach:</strong> Brad Shaw (Interim)</h3>
-                    <h3><strong>General Manager:</strong> Daniel Briere</h3>
+                    <h3><strong>Coach:</strong> <i>Rick Tocchet</i></h3>
+                    <h3><strong>General Manager:</strong> <i>Daniel Briere</i></h3>
                  </div>  
 
                 <img src="images/logo-flyers.png" alt="Title Logo">

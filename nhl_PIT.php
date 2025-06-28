db7e03b6fc90cdc8a8b555a413cac7f033792923
afrank14 <147450402+afrank14@users.noreply.github.com>
2025-06-28 17:56:55 -0600

    Update nhl_PIT.php

--- nhl_PIT.php
+++ nhl_PIT.php
@@ -68,8 +68,8 @@
                 <h2>Pittsburgh Penguins</h2>
 
                 <div class="team-info">
-                    <h3><strong>Coach:</strong> Vacant</h3>
-                    <h3><strong>General Manager:</strong> Kyle Dubas</h3>
+                    <h3><strong>Coach:</strong> <i>Dan Muse</i></h3>
+                    <h3><strong>General Manager:</strong> <i>Kyle Dubas</i></h3>
                  </div>  
 
                 <img src="images/logo-penguins.png" alt="Title Logo">

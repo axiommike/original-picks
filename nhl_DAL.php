45ce295b7bd07a229766d891beba8dc3f1800515
afrank14 <147450402+afrank14@users.noreply.github.com>
2025-06-28 17:45:36 -0600

    Update nhl_DAL.php

--- nhl_DAL.php
+++ nhl_DAL.php
@@ -68,8 +68,8 @@
                 <h2>Dallas Stars</h2>
 
                 <div class="team-info">
-                    <h3><strong>Coach:</strong> Peter DeBoer</h3>
-                    <h3><strong>General Manager:</strong> Jim Nill</h3>
+                    <h3><strong>Coach:</strong> <i>VACANT</i></h3>
+                    <h3><strong>General Manager:</strong> <i>Jim Nill</i></h3>
                  </div>  
 
                 <img src="images/logo-stars.png" alt="Title Logo">

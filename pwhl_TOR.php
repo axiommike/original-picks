a3344e4c1a3f05e453ab748927bb1f04dba78d20
afrank14 <147450402+afrank14@users.noreply.github.com>
2025-06-29 15:14:25 -0600

    Update pwhl_TOR.php

--- pwhl_TOR.php
+++ pwhl_TOR.php
@@ -68,8 +68,8 @@
                 <h2>Toronto Sceptres</h2>
 
                 <div class="team-info">
-                    <h3><strong>Coach:</strong> Troy Ryan</h3>
-                    <h3><strong>General Manager:</strong> Gina Kingsbury</h3>
+                    <h3><strong>Coach:</strong> <i>Troy Ryan</i></h3>
+                    <h3><strong>General Manager:</strong> <i>Gina Kingsbury</i></h3>
                  </div>  
 
                 <img src="images/logo-sceptres.png" alt="Title Logo">

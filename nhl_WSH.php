4f8b0e45c353ffa1b1ca0332dd15f982f59481f4
afrank14 <147450402+afrank14@users.noreply.github.com>
2025-06-28 18:03:25 -0600

    Update nhl_WSH.php

--- nhl_WSH.php
+++ nhl_WSH.php
@@ -68,8 +68,8 @@
                 <h2>Washington Capitals</h2>
 
                 <div class="team-info">
-                    <h3><strong>Coach:</strong> Spencer Carbery</h3>
-                    <h3><strong>General Manager:</strong> Chris Patrick</h3>
+                    <h3><strong>Coach:</strong> <i>Spencer Carbery</i></h3>
+                    <h3><strong>General Manager:</strong> <i>Chris Patrick</i></h3>
                  </div>  
 
                 <img src="images/logo-capitals.png" alt="Title Logo">

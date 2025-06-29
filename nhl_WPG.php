127b8d683c25bddcc7b2dd6523eeef3dd1f8be1c
afrank14 <147450402+afrank14@users.noreply.github.com>
2025-06-28 18:05:06 -0600

    Update nhl_WPG.php

--- nhl_WPG.php
+++ nhl_WPG.php
@@ -41,8 +41,8 @@
 
         /* Image Size */
         .title-logo img {
-            width: 120px; 
-            height: 90px; 
+            width: 100px; 
+            height: 100px; 
         }
     </style>
 

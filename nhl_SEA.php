1ad4771618326f30c427b437e17bd886efae3d0f
afrank14 <147450402+afrank14@users.noreply.github.com>
2025-08-23 09:16:17 -0600

    Update nhl_SEA.php

--- nhl_SEA.php
+++ nhl_SEA.php
@@ -67,6 +67,10 @@
             <div class="title-logo">
                 <h2>Seattle Kraken</h2>
 
+                <!--====== Contracts and Cap Hit Tracker ======-->
+                <?php $teamName = 'Seattle Kraken'; ?>
+                <?php include 'contracts_caphit.php'; ?>
+
                 <div class="team-info">
                     <h3><strong>Coach:</strong> <i>Lane Lambert</i></h3>
                     <h3><strong>General Manager:</strong> <i>Jason Botterill</i></h3>
@@ -74,6 +78,10 @@
 
                 <img src="images/logo-kraken.png" alt="Title Logo">
             </div>
+
+            <!--====== Average Ratings ======-->
+            <?php $teamName = 'Seattle Kraken'; ?>
+            <?php include 'average_ratings.php'; ?>
 
             <!--====== Table Pagination ======-->
             <div class="w3-bar w3-transparent">
@@ -95,6 +103,7 @@
 
     <!--====== JS ======-->
     <script src="js/navbar.js"></script>
+    <script src="js/extras.js"></script>
 
 </body>
 </html>

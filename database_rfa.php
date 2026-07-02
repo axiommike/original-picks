015d6b7df45ce88737b06f8d04c8531882c35f97
afrank14 <147450402+afrank14@users.noreply.github.com>
2026-07-02 14:08:17 -0600

    Update database_rfa.php

--- database_rfa.php
+++ database_rfa.php
@@ -72,8 +72,8 @@
 
             <!--====== Table Pagination ======-->
             <div class="w3-bar w3-transparent">
-                <a href="database_nhl.php" class="w3-button w3-right w3-text-black">NHL Database &#10095;</a>
-                <a href="database_pwhl.php" class="w3-button w3-text-black">&#10094; PWHL Database</a>
+                <a href="database_nhl.php" class="w3-button w3-text-black">&#10094; NHL Player Database</a>
+                <a href="database_fa.php" class="w3-button w3-right w3-text-black">Free Agent Database &#10095;</a>
             </div>
 
             <!--====== Database Table ======-->

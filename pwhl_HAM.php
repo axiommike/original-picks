ec6d2af01fa5265b20d6b13d58811d85fad98100
afrank14 <147450402+afrank14@users.noreply.github.com>
2026-07-10 22:42:16 -0600

    Update pwhl_HAM.php

--- pwhl_HAM.php
+++ pwhl_HAM.php
@@ -36,7 +36,7 @@
 
         .custom-table th {
             color: #fff; /* Change to your desired text color */
-            background-color: #0f4777;
+            background-color: #5e0a19;
         }
 
         /* Image Size */

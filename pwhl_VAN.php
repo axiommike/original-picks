cab60f72ee8a5f5e48d73ff3783a0bd8a92abf06
afrank14 <147450402+afrank14@users.noreply.github.com>
2026-07-10 18:13:19 -0600

    Update pwhl_VAN.php

--- pwhl_VAN.php
+++ pwhl_VAN.php
@@ -68,7 +68,7 @@
                 <h2>PWHL Vancouver</h2>
 
                 <div class="team-info">
-                    <h3><strong>Coach:</strong> <i>Brian Idalski</i></h3>
+                    <h3><strong>Coach:</strong> <i>VACANT</i></h3>
                     <h3><strong>General Manager:</strong> <i>Cara Gardner Morey</i></h3>
                  </div>  
 
